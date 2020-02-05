2ae70c674473fcf2fe87e05a5ac5752a0d7244d2
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-02-05 12:14:09 -0500

    add read to resource

--- src/resource.ts
+++ src/resource.ts
@@ -28,14 +28,54 @@ export default class Resource {
   /**
    * read
    */
-  public async read(nodeId: number | null, revision: Revision | [Revision, Revision], maxLevel: number | null = null) {
+  public async read(
+    nodeId: number | null,
+    revision: Revision | [Revision, Revision] | null,
+    maxLevel: number | null = null,
+    withMetadata: boolean = false
+  ): Promise<string> {
     if (!this.exists) {
       let created = await this.create();
       if (!created) {
         return null;
       }
     }
-    /// TODO!!
+    let params = {}
+    if (nodeId) {
+      params['nodeId'] = nodeId;
+    }
+    if (maxLevel) {
+      params['maxLevel'] = maxLevel;
+    }
+    if (withMetadata) {
+      params['withMetadata'] = true;
+    }
+    if (revision) {
+      if (typeof revision === 'number') {
+        params['revision'] = revision;
+      } else if (revision instanceof Date) {
+        params['revision-timestamp'] = revision.toISOString();
+      } else if (typeof revision[0] === 'number' || typeof revision[1] === 'number') {
+        params['start-revision'] = revision[0];
+        params['end-revision'] = revision[1];
+      } else {
+        params['start-revision-timestamp'] = revision[0].toISOString();
+        params['end-revision-timestamp'] = revision[1].toISOString();
+      }
+    }
+    let res = await Axios.get(
+      `${this.sirixInfo.sirixUri}/${this.dbName}/${this.resourceName}`,
+      {
+        params: params,
+        headers: { Authorization: this.authData.access_token, 'Content-Type': contentType(this.type) }
+      }
+    )
+    if (res.status !== 200) {
+      console.error(res.status, res.data);
+      return null;
+    } else {
+      return res.data;
+    }
   }
   /**
    * update
