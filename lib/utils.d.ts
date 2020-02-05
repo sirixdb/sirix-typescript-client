730d788756041cfdbf973a3cd9dda3770de15c2b
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-02-05 17:47:08 -0500

    fix tsconfig

--- lib/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function contentType(type: string): "application/json" | "application/xml";
-export declare function updateData(updated: any, old: any): void;
-export declare enum Insert {
-    CHILD = "asFirstChild",
-    LEFT = "asLeftSibling",
-    RIGHT = "asRightSibling",
-    REPLACE = "replace"
-}
