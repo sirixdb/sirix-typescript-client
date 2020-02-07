b824ebc008e2f849ffc85932d0dcdebc4aef7607
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-02-07 10:10:49 -0500

    change authenticate to use callback not async

--- src/auth.ts
+++ src/auth.ts
@@ -5,13 +5,7 @@ import { SirixInfo, LoginInfo, AuthData } from './info'
 
 export default class Auth {
   constructor(private loginInfo: LoginInfo, private sirixInfo: SirixInfo, private authData: AuthData, public callback: Function) {
-    this.authenticate().then(result => {
-      if (result) {
-        this._ready = true;
-      } else {
-        this._ready = false;
-      }
-    });
+    this.authenticate();
   }
   private timeout: any;
   private _ready: boolean = null;
@@ -23,20 +17,24 @@ export default class Auth {
       return this._ready;
     } else {
       await new Promise(r => setTimeout(r, 100));
-      return this.ready()
+      return this.ready();
     }
   }
-  public async authenticate() {
-    let res = await Axios.post(`${this.sirixInfo.sirixUri}/token`,
-      { username: this.loginInfo.username, password: this.loginInfo.password, grant_type: 'password' });
-    if (res.status >= 400) {
-      console.error(res.status, res.data);
-      return false;
-    } else {
-      updateData(res.data, this.authData);
-      this.setRefreshTimeout();
-      return true;
-    }
+  public authenticate() {
+    return Axios.post(`${this.sirixInfo.sirixUri}/token`,
+      { username: this.loginInfo.username, password: this.loginInfo.password, grant_type: 'password' })
+      .then(res => {
+        if (res.status >= 400) {
+          console.error(res.status, res.data);
+          this._ready = false;
+          return false;
+        } else {
+          updateData(res.data, this.authData);
+          this.setRefreshTimeout();
+          this._ready = false
+          return true;
+        }
+      })
   }
   private setRefreshTimeout() {
     this.timeout = setTimeout(() => this.refresh(), (this.authData.expires_in - 5) * 1000);
