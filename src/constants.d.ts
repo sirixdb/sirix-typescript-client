8d0842a8991e4e97ebfb0cc9b96efbfaf20f7a6d
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-06-04 10:19:43 -0400

    udpate

--- src/constants.d.ts
+++
@@ -1,8 +0,0 @@
-import { ContentType, DBType } from "./info";
-export declare function contentType(dbType: DBType): ContentType;
-export declare enum Insert {
-    CHILD = "asFirstChild",
-    LEFT = "asLeftSibling",
-    RIGHT = "asRightSibling",
-    REPLACE = "replace"
-}
