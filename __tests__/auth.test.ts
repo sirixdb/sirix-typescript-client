98f1c825fb2702dc123c87adface61ad9bbe6d31
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-09 12:07:45 -0400

    Update auth.test.ts

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -1,15 +1,16 @@
 import {mocked} from "ts-jest/utils";
+
 let mockedFetch = jest.fn();
 jest.mock('fetch-ponyfill', () => {
     return () => ({fetch: mockedFetch})
 });
 const Response = jest.requireActual('fetch-ponyfill')().Response;
-import fetchPonyfill from "fetch-ponyfill";
-const {fetch} = fetchPonyfill();
+mockedFetch = mocked(mockedFetch, true);
+
 import Client from "../src/client";
 import {initClient} from '../src/auth'
 
-let token = {
+export let token = {
     "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJCU0Q2b0xMdkIzUDdYWmNZQzVBUE5TUHQ0VGJCNURDbnF2UmViOF9XODM4In0.eyJqdGkiOiJlNWUxODhhMC03NjY4LTQwN2UtOGViMy03Njc0MDkxMGVhNzYiLCJleHAiOjE1ODkzMjk0MDAsIm5iZiI6MCwiaWF0IjoxNTg5MzI5MTAwLCJpc3MiOiJodHRwOi8va2V5Y2xvYWs6ODA4MC9hdXRoL3JlYWxtcy9zaXJpeGRiIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjIwNjIyOTQwLTE4ODMtNGYwMC1iN2ZlLTAxNjAzZTZmZmZkNyIsInR5cCI6IkJlYXJlciIsImF6cCI6InNpcml4IiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiY2EwMGEyMzEtNzlmNy00MDUwLWI0ODYtYWQ3M2FhMjcwYzE1IiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJtb2RpZnkiLCJ2aWV3Iiwib2ZmbGluZV9hY2Nlc3MiLCJjcmVhdGUiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlbGV0ZSJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4ifQ.MQLlTHHcU1WboWUzC3qKmdhw1Hvr1pJUePIzPGV7agSBC_WCcUZIXzjZKfU5A9oejQSQthYAzxEwsrIjMhU6vMS9uR9jYSl82ES9GF7hpsuXWM7yvkpNHHYWk0YdWoIi9QdJ__JiZm7LZJntZWPCQS8j0XChjTyc2eizDRlPT5R_8FpPkLDd4qy73QtvbrBzFS5XyRzINYegsU4ZJ-R49BMOfDncEOVYcXd4O4cpJdLRVEV5-s6YA_Yi2-xqO1hT4nECN3lO8-5D2JGRSCjCpcr047QLVZ5e3oLbCGVPuyokNUjhZ46rkdLMgN9UJnbBdcraEUPWvO-tmUPODEu5Vw",
     "expires_in": 11,
     "refresh_expires_in": 1800,
@@ -20,9 +21,6 @@ let token = {
     "scope": "profile email",
     "expires_at": 1589329400037
 };
-
-mockedFetch = mocked(mockedFetch, true);
-
 
 describe("test authentication", () => {
     beforeEach(() => {
@@ -37,7 +35,7 @@ describe("test authentication", () => {
         let sentUrl: string;
         let sentData: string;
         const m = mockedFetch.mockImplementation(async (requestInfo: RequestInfo,
-                                                              requestInit: RequestInit): Promise<Response> => {
+                                                        requestInit: RequestInit): Promise<Response> => {
             sentUrl = requestInfo as string;
             sentData = requestInit.body as string;
             return new Response(JSON.stringify(token), {status: 200});
