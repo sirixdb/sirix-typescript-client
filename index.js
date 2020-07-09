44216a3f9e46faeedc42b6908631e55ae80594de
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-09 11:47:15 -0400

    configure setup

--- index.js
+++
@@ -1,5 +0,0 @@
-import { Sirix, sirixInit } from "./src/sirix";
-import Database from "./src/database";
-import Resource from "./src/resource";
-import { Insert } from "./src/constants";
-export default { Sirix: Sirix, Database: Database, Resource: Resource, sirixInit: sirixInit, Insert: Insert };
