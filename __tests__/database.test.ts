a0cb3f05e959d7746b12ca8e3fb9da7283826aeb
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-09 12:07:49 -0400

    Update database.test.ts

--- __tests__/database.test.ts
+++ __tests__/database.test.ts
@@ -1,25 +1,50 @@
+import {mocked} from "ts-jest/utils";
+let mockedFetch = jest.fn();
+jest.mock('fetch-ponyfill', () => {
+    return () => ({fetch: mockedFetch})
+});
+const Response = jest.requireActual('fetch-ponyfill')().Response;
+mockedFetch = mocked(mockedFetch, true);
+
 import {Sirix, sirixInit} from "../src/sirix";
 import Database from "../src/database";
 import {DBType} from "../src/info";
 import Resource from "../src/resource";
+import {token} from "./auth.test";
+
 
 describe('test Database class', () => {
     let sirix: Sirix;
     let db: Database;
 
     beforeEach(async () => {
+        mockedFetch.mockImplementationOnce(async (requestInfo: RequestInfo,
+                                                  requestInit: RequestInit): Promise<Response> => {
+            return new Response(JSON.stringify(token), {status: 200});
+        });
         sirix = await sirixInit("http://localhost:9443",
             {username: "admin", password: "admin"});
-        await sirix.deleteAll();
         db = sirix.database("testing", DBType.JSON);
     });
     afterEach(async () => {
-        await sirix.deleteAll();
         sirix.shutdown();
     });
 
     test('Database.create() and getInfo', async () => {
+        mockedFetch.mockImplementationOnce(async (requestInfo: RequestInfo,
+                                                  requestInit: RequestInit): Promise<Response> => {
+            expect(requestInfo as string).toEqual("http://localhost:9443/testing");
+            expect(requestInit.method).toEqual("PUT");
+            return new Response(null, {status: 200});
+        });
         await db.create();
+        mockedFetch.mockImplementationOnce(async (requestInfo: RequestInfo, requestInit: RequestInit): Promise<Response> => {
+            expect(requestInfo as string).toEqual("http://localhost:9443/testing");
+            expect(requestInit.method).toEqual("GET");
+            return new Response(JSON.stringify({
+                "resources": []
+            }), {status: 200});
+        });
         const info = await db.getInfo();
         expect(info).toEqual({
             "resources": []
@@ -27,9 +52,12 @@ describe('test Database class', () => {
     });
 
     test('Database.delete()', async () => {
-        await db.create();
+        mockedFetch.mockImplementationOnce(async (requestInfo: RequestInfo, requestInit: RequestInit): Promise<Response> => {
+            expect(requestInfo as string).toEqual("http://localhost:9443/testing");
+            expect(requestInit.method).toEqual("DELETE");
+            return new Response(null, {status: 200});
+        });
         await db.delete();
-        await expect(db.getInfo()).rejects.toThrow();
     });
 
     test('Database.resources()', () => {
