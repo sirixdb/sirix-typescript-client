3349f0e2241cb5d4ddc5df99ed0e7232cc0db6e7
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-19 13:02:39 -0400

    change export

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import { Sirix, sirixInit } from "./src/sirix";
 import Database from "./src/database";
 import Resource from "./src/resource";
 import { Insert } from "./src/constants"
-export default { Sirix, Database, Resource, sirixInit, Insert };
+export { Sirix, Database, Resource, sirixInit, Insert };
